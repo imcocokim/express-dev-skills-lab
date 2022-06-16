c66c98708ed88d74673301d039ef121d4ee26bff
imcoco <imcoco@gmail.com>
2022-06-16 09:11:42 -0400

    change modules to restaurants

---
+++ data/restaurant-data.js
@@ -0,0 +1,12 @@
+const restaurants = [
+  {text: 'Huertas', visit: true, _id: 125223},
+  {text: 'Don Angie', visit: false, _id: 125224},
+  {text: 'Popina', visit: true, _id: 125225},
+  {text: 'Rowdy Rooster', visit: true, _id: 125226},
+  {text: 'COTE', visit: false, _id: 125227},
+
+]
+
+export { 
+	restaurants
+}
